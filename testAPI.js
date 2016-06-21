4d2fdf046c20d35dfd41f946e24fbcde601feee3
倪思宇 <nisiyu@nisiyudeMacBook-Pro.local>
2016-06-22 01:40:21 +0800

    三条路线的结果呈现

--- testAPI.js
+++ testAPI.js
@@ -1,5 +1,7 @@
 //console.log(verticesstr);
 var map;
+//三条线的颜色, 可以修改
+var colors = ["#3366FF", "#ff5330", "#ed32ff"];
 var mapfunc = { 
     init: function(win){
     map = new AMap.Map('container',{
@@ -13,99 +15,108 @@ var mapfunc = {
     var scale = new AMap.Scale();
     map.addControl(toolBar);
     map.addControl(scale);
+    
+    });
+    //初始化时设定3个路线的颜色
+    $(".input>span").each(function(index, element) {
+        $(element).css("color", colors[index]);
     });
 },
 
 searchPath: function(win){
-var start = document.getElementById("start").value;
-var end = document.getElementById("end").value;
-//设定在container中添加map
 
 
-function getPosList(stable, vertices, end) {
-    var res = [];
-    if (stable[end]['last']) {
-        res = getPosList(stable, vertices, stable[end]['last'])
-        res.push([parseFloat(vertices[end]['x']), parseFloat(vertices[end]['y'])]);
-        return res;
-    }else {
-        return [[parseFloat(vertices[end]['x']),parseFloat(vertices[end]['y'])]];
+    function getPosList(stable, vertices, end) {
+        var res = [];
+        var resstr = "";
+        if (stable[end]['last']) {
+            var robj = getPosList(stable, vertices, stable[end]['last'])
+            res = robj['pos'];
+            resstr = robj['str']
+            res.push([parseFloat(vertices[end]['x']), parseFloat(vertices[end]['y'])]);
+            resstr += '->' + end
+            return {
+                str: resstr,
+                pos: res};
+        }else {
+            return {
+                str: end,
+                pos: [[parseFloat(vertices[end]['x']),parseFloat(vertices[end]['y'])]]
+            };
+        }
     }
-}
 
-function findPath(vertices, start, end){
-    var stable = {};
-    var unstable = {};
-    stable[start] = {dist:0};
-    var point = start;
-    var nowdist = 0;
-    if (((vertices[start]) && (vertices[end])) == false) {
-        return [];
-    }
-    while (1) {
-        var neighbors = vertices[point]['neighbors'];
-        for (var i in neighbors) {
-            if (stable[i])
-                continue;
+    function findPath(vertices, start, end){
+        var stable = {};
+        var unstable = {};
+        stable[start] = {dist:0};
+        var point = start;
+        var nowdist = 0;
+        if (((vertices[start]!== undefined) && (vertices[end]!==undefined)) === false) {
+            return [];
+        }
+        while (1) {
+            var neighbors = vertices[point]['neighbors'];
+            for (var i in neighbors) {
+                if (stable[i])
+                    continue;
 
-            var dist = neighbors[i]['dist'] + nowdist;
-            if ((! unstable[i]) || (dist < unstable[i]['dist'])) {
-                unstable[i] = {dist:dist, 
-                               last:point};
-                //console.log(unstable[i]);
+                var dist = neighbors[i]['dist'] + nowdist;
+                if ((! unstable[i]) || (dist < unstable[i]['dist'])) {
+                    unstable[i] = {dist:dist, 
+                                   last:point};
+                    //console.log(unstable[i]);
+                }
             }
-        }
-        var smallest = 10;
-        var tobestableobj = {};
-        var tobestablename = '';
-        for (var i in unstable) {
-            if ((unstable[i]) && (unstable[i]['dist'] < smallest)) {
-                smallest = unstable[i]['dist'];
-                tobestablename = i;
-                tobestableobj = unstable[i];
+            var smallest = 10;
+            var tobestableobj = {};
+            var tobestablename = '';
+            for (var i in unstable) {
+                if ((unstable[i]) && (unstable[i]['dist'] < smallest)) {
+                    smallest = unstable[i]['dist'];
+                    tobestablename = i;
+                    tobestableobj = unstable[i];
+                }
             }
-        }
 
-        stable[tobestablename] = tobestableobj;
-        unstable[tobestablename] = undefined;
+            stable[tobestablename] = tobestableobj;
+            unstable[tobestablename] = undefined;
 
-        if (end === tobestablename) {
-            return getPosList(stable, vertices, end);
+            if (end === tobestablename) {
+                return getPosList(stable, vertices, end);
+            }
+                
+            point = tobestablename;
+            nowdist = smallest;
         }
-            
-        point = tobestablename;
-        nowdist = smallest;
     }
-}
-var lineArr = findPath(verticesstr,start,end);
-//lineArr保存的是各个站点的坐标
-// var lineArr = [[121.533168, 31.295784],
-// [121.534941, 31.288543],
-// [121.528401, 31.278814],
-// [121.518379, 31.274946],
-// [121.509685, 31.273242],
-// [121.501488, 31.27488],
-// [121.494362, 31.268533],
-// [121.488669, 31.259278],
-// [121.484208, 31.252035],
-// [121.482473, 31.243829],
-// [121.483609, 31.237101],
-// [121.475719, 31.23277]];
-
-// 比例尺插件增加
-
-
-//polyline设置要画的折线
-var polyline = new AMap.Polyline({
-        path: lineArr,          //设置线覆盖物路径
-        strokeColor: "#3366FF", //线颜色
-        strokeOpacity: 1,       //线透明度
-        strokeWeight: 10,        //线宽
-        strokeStyle: "solid",   //线样式
-        strokeDasharray: [10, 5] //补充线样式
+    var start = [];
+    var end = [];
+    $(".input>.start").each(function(){
+        start.push($(this).val());
     });
+    $(".input>.end").each(function(){
+        end.push($(this).val());
+    });
+    for (var i=0; i<3; i++) {
+        var pathObj = findPath(verticesstr,start[i],end[i]);
+        var lineArr = pathObj['pos'];
+        var linestr = pathObj['str'];
+        $(".input:nth-child("+(i+2).toString()+")>p").text(linestr);
 
-//画线
-polyline.setMap(map);
+        //polyline设置要画的折线
+        var polyline = new AMap.Polyline({
+                path: lineArr,          //设置线覆盖物路径
+                strokeColor: colors[i], //线颜色
+                strokeOpacity: 1,       //线透明度
+                strokeWeight: 10,        //线宽
+                strokeStyle: "solid",   //线样式
+                strokeDasharray: [10, 5] //补充线样式
+            });
+
+        //画线
+        polyline.setMap(map);
+    }
+
 }
 };
